6f8f5dc7179a6f4f1394c40e3650ac1c73b48a00
huyixi <hi@huyixi.com>
2025-09-18 21:47:10 +0800

    chore

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -94,10 +94,7 @@ export function LoginForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <Link
-                href="/auth/sign-up"
-                className="underline underline-offset-4"
-              >
+              <Link href="/sign-up" className="underline underline-offset-4">
                 Sign up
               </Link>
             </div>
